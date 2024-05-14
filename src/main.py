b027dba72b100bf4f06383bf2fca3a60e661eb93
Julian Sarmiento <juliansarmiento10@outlook.com>
2024-05-14 17:33:43 -0500

    modify main with instance EncryptData obj

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
-from AESEncryption import AESEncryption
+from EncryptData import EncryptData
 
-if __name__ == "__main__":    
-    # NIST AES-128 test vector 1 (Ch. C.1, p. 35)
-    encrypt = AESEncryption()
+if __name__ == "__main__":  
+    generic_polynomial = '1+x+x**3+x**4+x**8'
     plaintext = bytearray.fromhex('00112233445566778899aabbccddeeff')
     key = bytearray.fromhex('000102030405060708090a0b0c0d0e0f')
-    expected_ciphertext = bytearray.fromhex('69c4e0d86a7b0430d8cdb78070b4c55a')
-    ciphertext = encrypt.aes_encryption(plaintext, key)        
-    print(ciphertext, " aleluya ", expected_ciphertext)
+    encrypt1 = EncryptData(plaintext, key, generic_polynomial)
+    encrypt1.encrypt()
+    encrypt1.avalanche()

a68d55f2fba6b29e8aafca1c99b18643a1c01f69
Julian Sarmiento <juliansarmiento10@outlook.com>
2024-05-14 14:26:44 -0500

    add key expansion

---
+++ src/KeyExpansion.py
@@ -0,0 +1,20 @@
+from AESOperations import AESOperations
+
+class KeyExpansion:
+    @staticmethod
+    def generate_key(key: bytes, nb: int = 4) -> list[list[list[int]]]:
+        aes = AESOperations() 
+        nk = len(key) // 4    
+        w = aes.state_from_bytes(key)
+        for i in range(nk, nb * (10 + 1)):
+            temp = w[i-1]
+            if i % nk == 0:
+                temp = aes.xor_bytes(aes.sub_word(aes.rot_word(temp)), aes.rcon(i // nk))
+            elif nk > 6 and i % nk == 4:
+                temp = aes.sub_word(temp)
+            w.append(aes.xor_bytes(w[i - nk], temp))           
+        return [w[i*4:(i+1)*4] for i in range(len(w)//4)]
+
+# 
+# key = bytearray.fromhex('000102030405060708090a0b0c0d0e0f')
+# print(KeyExpansion.generate_key(key))

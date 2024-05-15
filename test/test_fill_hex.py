9c9f492b8f7701837099a9eacfb103d6b75bd63e
Julian Sarmiento <juliansarmiento10@outlook.com>
2024-05-15 00:44:42 -0500

    correct test

--- test/test_fill_hex.py
+++ test/test_fill_hex.py
@@ -3,4 +3,4 @@ from src.util.FillHex import FillHex
 def test_fill_hex_with_zero():
     string_hex = '0123456789abcdeffedcba'
     result = FillHex.complete_hex_with_zero(string_hex)
-    assert result == '0123456789abcdeffedcba00000000000000000000'
+    assert result == '0123456789abcdeffedcba0000000000'

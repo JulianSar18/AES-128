1b3a917e5004eb921c39cbbc62e0988906872c65
Julian Sarmiento <juliansarmiento10@outlook.com>
2024-05-15 00:42:30 -0500

    change method to fill hex

--- src/util/FillHex.py
+++ src/util/FillHex.py
@@ -1,7 +1,7 @@
 class FillHex:
     @staticmethod
-    def complete_hex_with_zero(hex_string : str):
+    def complete_hex_with_zero(hex_string: str):
         currently_length = len(hex_string.replace(" ", ""))
-        missing_bytes = (16 - (currently_length % 16)) % 16
-        complete_hex = hex_string + "00" * missing_bytes
+        missing_bytes = 32 - currently_length
+        complete_hex = hex_string + "0" * missing_bytes
         return complete_hex
